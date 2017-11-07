6c32f1208ed5263ed5e4d821030547a48ab2cde5
Ed Rogers <etr@orc.soton.ac.uk>
2017-11-07 15:05:12 +0000

    Add GPL licensing

--- add_microglia_nearest_neighbour_info_to_files.m
+++ add_microglia_nearest_neighbour_info_to_files.m
@@ -1,3 +1,19 @@
+% ImageJ macro to process dual colour DAB/H images of microglia nd
+% calculate various properties.
+% Copyright (C) 2017 Edward Rogers
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 clear;
 
 path = uigetdir;
--- nearest_neighbour_distance.m
+++ nearest_neighbour_distance.m
@@ -1,4 +1,20 @@
 function nn_dist = nearest_neighbour_distance(xin,yin)
+% ImageJ macro to process dual colour DAB/H images of microglia nd
+% calculate various properties.
+% Copyright (C) 2017 Edward Rogers
+% 
+% This program is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% This program is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 points = [xin,yin];
     
